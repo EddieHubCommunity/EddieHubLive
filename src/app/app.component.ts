584f682704fc50a7071b237b83f81e9183c8892f
Eddie Jaoude <eddie@jaoudestudios.com>
2020-07-28 18:08:25 +0100

    feat(map): map, users, events displaying

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject, interval, timer, from, of } from 'rxjs';
-import { map, delay, bufferTime, switchMap, mergeMap, concatMap } from 'rxjs/operators';
+import { from, of, interval } from 'rxjs';
+import { map, delay, mergeMap, concatMap, tap } from 'rxjs/operators';
 import { icon, latLng, marker, tileLayer, TileLayer, Marker } from 'leaflet';
 import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase';
 
 interface User {
+  username: string;
   avatar: string;
+  roles: string[];
   bio: {
+    description: string;
+    github: string;
     location: {
+      display_name: string;
+      address: {
+        city: string;
+        country: string;
+      };
       lat: number,
       lon: number,
-    }
+    };
   };
+  updatedAt: firebase.firestore.Timestamp;
+}
+
+interface Event {
+  avatar: string;
+  username: string;
+  type: string;
+  createdAt: firebase.firestore.Timestamp;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
 
@@ -25,42 +43,82 @@ export class AppComponent implements OnInit {
     detectRetina: true,
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   });
+  private isActive = false;
 
-  public zoom = 3;
+  public icons = { regular: 'user' };
+  public zoom = 2;
   public center = latLng([ 48.8566969, 2.3514616 ]);
   public layers: (TileLayer | Marker)[] = [ this.streetMaps ];
+  public users: User[] = [];
+  public eventStream: Event[] = [];
 
-  constructor(firestore: AngularFirestore) {
-    firestore.collection<User>('users').stateChanges([ 'added', 'modified' ])
-      .pipe(
-        map(actions => actions.map(a => {
-          const data = a.payload.doc.data() as User;
-          const id = a.payload.doc.id;
-          return { id, ...data };
-        })),
-        map((users) => users.filter((user) => user.bio.location)),
-        mergeMap((user) => from(user)),
-        concatMap((user) => of(user).pipe(delay(2000)))
-      )
-      .subscribe((user) => {
-        // compare existing markers against new
-        console.log('USER', user);
+  constructor(angularFirestore: AngularFirestore) {
+    angularFirestore
+      .collection<Event>('eventStream', (ref) => ref.orderBy('createdAt', 'asc'))
+      .stateChanges([ 'added' ])
+        .pipe(
+          map(actions => actions.map(a => {
+            const data = a.payload.doc.data() as Event;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })),
+          mergeMap((event) => from(event)),
+          concatMap((event) => of(event).pipe(delay(500))),
+          tap((event) => console.log('EVENT', event))
+        )
+        .subscribe((event) => this.eventStream.unshift(event));
 
-        const point = marker([ user.bio.location.lat, user.bio.location.lon ], {
-          icon: icon({
-            iconSize: [ 100, 100 ],
-            iconAnchor: [ 13, 0 ],
-            iconUrl: user.avatar,
-          })
-        });
-        this.zoom = 7;
-        this.center = latLng([ user.bio.location.lat, user.bio.location.lon ]);
-        this.layers = [
-          ...this.layers,
-          point
-        ];
+    angularFirestore.collection<User>('users', (ref) => ref.orderBy('updatedAt', 'asc'))
+      .stateChanges([ 'added', 'modified' ])
+        .pipe(
+          map(actions => actions.map(a => {
+            const data = a.payload.doc.data() as User;
+            const id = a.payload.doc.id;
+            return { id, ...data };
+          })),
+          map((users) => users.filter((user) => user.bio.location)),
+          mergeMap((user) => from(user)),
+          concatMap((user) => of(user).pipe(delay(1000)))
+        )
+        .subscribe((user) => {
+          this.isActive = true;
+          this.users = this.users.filter((u) => u.username !== user.username);
+          this.users.unshift(user);
+          console.log('USER', user);
+
+          this.zoom = 7;
+          this.center = latLng([ user.bio.location.lat, user.bio.location.lon ]);
+          this.layers = [
+            this.streetMaps,
+            ...this.users.map((u) => marker([ u.bio.location.lat, u.bio.location.lon ], {
+              icon: icon({
+                iconSize: [ 100, 100],
+                iconAnchor: [ 50, 50 ],
+                iconUrl: u.avatar || 'https://raw.githubusercontent.com/eddiejaoude/eddiejaoude.github.com/master/img/chibi.png',
+              })
+            }))
+          ];
 
       });
+
+    interval(5000)
+        .subscribe(() => {
+          if (!this.isActive) {
+            this.zoom = 2;
+            this.center = latLng([ 48.8566969, 2.3514616 ]);
+            this.layers = [
+              this.streetMaps,
+              ...this.users.map((u) => marker([ u.bio.location.lat, u.bio.location.lon ], {
+                icon: icon({
+                  iconSize: [ 30, 30],
+                  iconAnchor: [ 50, 50 ],
+                  iconUrl: u.avatar || 'https://raw.githubusercontent.com/eddiejaoude/eddiejaoude.github.com/master/img/chibi.png',
+                })
+              }))
+            ];
+          }
+          this.isActive = false;
+        });
   }
 
   ngOnInit() {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { MomentModule } from 'ngx-moment';
 
 import { environment } from '../environments/environment';
 
@@ -15,11 +19,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule,
     LeafletModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
+    MomentModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private library: FaIconLibrary) {
+    library.addIcons(faUser);
+  }
+}
