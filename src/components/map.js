66ca407082adbb5c41ffb34ecb47e0bea6c2a04a
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-06-06 17:29:51 +0200

    feat: add tailwind (#6)

--- src/components/map.js
+++ src/components/map.js
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const map = () => {
