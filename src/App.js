b6ab8f6641a3ce4ef329610bb0459292824164fc
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-06-07 08:53:28 +0200

    refactor: websockets (#7)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import { SocketContext, socket } from "./context/socket";
+import { socket } from "./context/socket";
 import Map from "./components/map";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    socket.emit("event");
+    socket.on("events", (data) => {
+      setEvents([...events, data]);
+    });
+  });
+
   return (
-    <SocketContext.Provider value={socket}>
-      <Map />
-    </SocketContext.Provider>
+    <main className="flex">
+      <div className="w-1/3"></div>
+      <div className="w-2/3">
+        <Map />
+      </div>
+    </main>
   );
 }
 
--- src/context/socket.js
+++ src/context/socket.js
@@ -1,6 +1,4 @@
 import io from "socket.io-client";
-import { createContext } from "react";
 import { SOCKET_URL } from "../config";
 
 export const socket = io.connect(SOCKET_URL);
-export const SocketContext = createContext();
