f0950a630b2aa1b77f2f22310500518eacfb9dc3
Cahllagerfeld <43843195+Cahllagerfeld@users.noreply.github.com>
2022-06-09 16:09:59 +0000

    fix: transport

--- src/context/socket.js
+++ src/context/socket.js
@@ -1,4 +1,7 @@
 import io from "socket.io-client";
 import { SOCKET_URL } from "../config";
 
-export const socket = io.connect(SOCKET_URL);
+export const socket = io.connect(SOCKET_URL, {
+  // WARNING: in that case, there is no fallback to long-polling
+  transports: ["websocket", "polling"], // or [ "websocket", "polling" ] (the order matters)
+});
