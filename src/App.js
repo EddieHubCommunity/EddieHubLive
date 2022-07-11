179db9661f7c4012b63cdc78e6c75b9484ba534f
Brijesh Patadiya <51873202+brijeshsoni1300@users.noreply.github.com>
2022-07-11 19:23:17 +0530

    fix: multiple pics for same user on map (#37)
    * fix: multiple pics for same user on map
    
    * fix: indentation on map

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         ))}
       </div>
       <div className="w-2/3">
-        <Map events={events} />
+        <Map events={[...new Set(events)]} />
       </div>
     </main>
   );
