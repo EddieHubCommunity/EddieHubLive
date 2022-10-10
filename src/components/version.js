314e8a7b0d897114dd33ab790629548a23fd149d
Jaka Daneu <jakad@me.com>
2022-10-10 09:57:35 +0200

    refactor: ♻️ get app version from env

--- src/components/version.js
+++ src/components/version.js
@@ -1,10 +1,11 @@
-import packageJson from '../package.alias.json';
+
 export default function Version() {
+  const version = process.env.REACT_APP_VERSION
   return (
     <div className="leaflet-controlcontainer">
       <div className="leaflet-top leaflet-right">
         <div className="leaflet-control-attribution">
-          <span>v{packageJson.version}</span>
+          <span>v{version}</span>
         </div>
       </div>
     </div>
