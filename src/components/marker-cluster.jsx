513c35b24603528290e82004df0200a71bc7fabf
Amanda Martin <97615019+amandamartin-dev@users.noreply.github.com>
2022-12-18 19:21:31 -0500

    clean up

--- src/components/marker-cluster.jsx
+++ src/components/marker-cluster.jsx
@@ -9,25 +9,23 @@ function createMarkerCluster({ children: _c, ...props }, context) {
   const clusterEvents = {}
 
   Object.entries(props).forEach(([propName, prop]) =>
-    propName.startsWith('on')
-      ? (clusterEvents[propName] = prop)
-      : (clusterProps[propName] = prop)
-  )
-  const instance = new L.MarkerClusterGroup(clusterProps)
+    propName.startsWith('on') ? (clusterEvents[propName] = prop) : (clusterProps[propName] = prop)
+  );
+  const instance = new L.MarkerClusterGroup(clusterProps);
 
   Object.entries(clusterEvents).forEach(([eventAsProp, callback]) => {
     const clusterEvent = `cluster${eventAsProp.substring(2).toLowerCase()}`
     instance.on(clusterEvent, callback)
-  })
+  });
   return {
     instance,
     context: {
       ...context,
       layerContainer: instance,
     },
-  }
+  };
 }
 
-const MarkerCluster = createPathComponent(createMarkerCluster)
+const MarkerCluster = createPathComponent(createMarkerCluster);
 
-export default MarkerCluster
+export default MarkerCluster;
